5e9a1c3e69ee49966b93662832d20b492a17e1bd
Tamer Badarneh <tamerse21business@gmail.com>
2025-09-08 10:18:23 +0300

    First Commit: Application Setup

---
+++ src/test/java/org/selflearning/complaint_api/ComplaintApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.selflearning.complaint_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
